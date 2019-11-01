37575de6fcea4c833510114f9e96c5d9acf8616b
zip520123 <zip520123@yahoo.com.tw>
2019-11-01 14:29:25 +0000

    add single number set

--- array/singleNum.js
+++ array/singleNum.js
@@ -18,26 +18,35 @@ Example 2:
 Input: [4,1,2,1,2]
 Output: 4
  */
-var singleNumber = function(nums) {
-    var list = []
-    for (let i = 0; i < nums.length; i+=1) {
-        var char = nums[i]
-        //find char in list
-        var find = list.find((number) => {
-            return number == char
-        })
-        //if not find, add 
-        if (find == undefined) {
-            list.push(char)
-        } else {
-            //else remove exist
-            var index = list.indexOf(find)
-            list.splice(index, 1)
-        }
-    }
-    return list[0]
-};
+// list
+// var singleNumber = function(nums) {
+//     var list = []
+//     for (let i = 0; i < nums.length; i+=1) {
+//         var char = nums[i]
+//         //find char in list
+//         var find = list.find((number) => {
+//             return number == char
+//         })
+//         //if not find, add 
+//         if (find == undefined) {
+//             list.push(char)
+//         } else {
+//             //else remove exist
+//             var index = list.indexOf(find)
+//             list.splice(index, 1)
+//         }
+//     }
+//     return list[0]
+// };
 
+// set
+var singleNumber = function(nums) {
+    // set(nums) * 2 - nums
+    let set = new Set(nums)
+    let arr1 = [...set].reduce((p,c) => {return p + c})
+    let arr2 = nums.reduce((p,c) => {return p + c})
+    return 2 * arr1 - arr2
+}
 console.log(singleNumber([2,2,1]) == 1)
 console.log(singleNumber([1,0,1]) == 0)
 console.log(singleNumber([4,1,2,1,2]) == 4)
