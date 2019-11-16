dcdbeb15b133d8c5f111038eecf99d7a2af98358
zip520123 <zip520123@yahoo.com.tw>
2019-11-16 20:13:05 +0000

    update Binary Tree Level Order Traversal Solution

--- tree/Binary Tree Level Order Traversal Solution.js
+++ tree/Binary Tree Level Order Traversal Solution.js
@@ -21,80 +21,35 @@ function TreeNode(val,left,right) {
      this.right = right;
 }
 var levelOrder = function(root) {
-  res = []
-  nodes = []
-  //check root
+  let res = []
+  let nodes = []
+  // if (root) nodes.push(root)
   if (root) nodes.push(root)
-  //while nodes.length > 0
-  while(nodes.length > 0) {
-    // newArr = []
-    newArr = []
-    // pop current node in nodes
-    // let len = nodes.len
-    // while len
+  // while(nodes.length > 0)
+  while(nodes.length > 0){
+    //  let len = nodes.length
     let len = nodes.length
-    while (len > 0){
-      len --
-      //   newArr.push node.val
-      node = nodes.shift()
-      newArr.push(node.val)
-      //   if node.left push nodes.left nodes
-      if (node.left) nodes.push(node.left)
-      //   if node.right push nodes.right nodes
-      if (node.right) nodes.push(node.right)
-
+  //  temp = []
+    let temp = []
+  //  while(len > 0)
+    while(len > 0){
+      //   len--
+      len--
+  //   node = nodes.shift()
+      let node = nodes.shift()
+  //   if node temp.push(node.val)
+      if (node) temp.push(node.val)
+  //   if node.left nodes push(node.left)
+      if(node.left)nodes.push(node.left)
+  //   if node.right nodes.push(nodes.right)
+      if(node.right)nodes.push(node.right)
     }
-    // res push newArr
-    res.push(newArr)
+  
+  // res.push(temp)
+    res.push(temp)
   }
+  
   //return res
   return res
 }
-
-// var levelOrder = function(root) {
-//   let res = []
-//   let nodes = []
-//   // check
-//   if (root) nodes.push(root)
-//   //while node.length > 0
-//   while( nodes.length > 0) {
-//     // newArr = []
-//     newArr = []
-//     let len = nodes.length 
-//     // current node in nodes
-//     while(len > 0) {
-//       len -= 1
-//       // push node.val 
-//       let node = nodes.shift()
-//       newArr.push(node.val)
-      
-//       // push node.left node.right nodes
-//       if (node.left) nodes.push(node.left)
-//       if (node.right) nodes.push(node.right)
-//     }
-//     //res push newArr
-//     res.push(newArr)
-//   }
-//   return res
-// }
-
-// var levelOrder = function(root) {
-//   let res = []
-//   nodes = []
-//   if (root) nodes.push(root)
-
-//   while (nodes.length > 0) {
-//     current = []
-//     let len = nodes.length
-//     for (i = 0; i< len; i+=1) {
-//       let node = nodes.shift()
-//       current.push(node.val)
-//       if (node.left) nodes.push(node.left)
-//       if (node.right) nodes.push(node.right)
-
-//     }
-//     res.push(current)
-//   }
-//   return res
-// }
 
