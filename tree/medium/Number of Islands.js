3ef678dbfe4f47d4089f4005bca82b395f425ce7
zip520123 <zip520123@yahoo.com.tw>
2019-12-07 17:49:00 +0000

    add number of islands iterative

--- tree/medium/Number of Islands.js
+++ tree/medium/Number of Islands.js
@@ -45,3 +45,81 @@ const bfs = (grid,i,j) => {
   bfs(grid,i+1,j)
   bfs(grid,i,j-1)
 }
+
+// iterative
+var Node = function(i,j) {
+  this.i = i
+  this.j = j
+}
+
+var numIslands = function(grid) {
+  res = 0
+  for(i=0;i<grid.length;i++){
+    for(j=0;j<grid[i].length;j++){
+       if (grid[i][j] == 1){
+         res ++
+         stack = [new Node(i,j)]
+         for(k=0;k<stack.length;k++){
+           let node = stack[k]
+           if (node.i >= 0 && node.j>= 0 && node.i < grid.length && node.j < grid[i].length && grid[node.i][node.j]==1){
+             grid[node.i][node.j] = 0
+
+             stack.push(new Node(node.i + 1,node.j), new Node(node.i, node.j + 1), new Node(node.i - 1,node.j), new Node(node.i,node.j - 1))
+           }
+         }
+       }
+    }
+  }
+  return res
+}
+
+
+/*def numIslands(self, grid):
+    # BFS
+    if not grid:
+        return 0
+    m, n, count = len(grid), len(grid[0]), 0
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == "1":
+                count += 1
+                stack = [(i,j)]
+                for ii,jj in stack:
+                    if 0<=ii<m and 0<=jj<n and grid[ii][jj] == "1":
+                        grid[ii][jj] = "2"
+                        stack.extend([(ii+1,jj),(ii-1,jj),(ii,jj-1),(ii,jj+1)])
+    return count
+
+    # DFS
+    if not grid:
+        return 0
+    m, n, count = len(grid), len(grid[0]), 0
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == "1":
+                count += 1
+                stack = [(i,j)]
+                while stack:
+                    ii, jj = stack.pop()
+                    if 0<=ii<m and 0<=jj<n and grid[ii][jj] == "1":
+                        grid[ii][jj] = "2"
+                        stack.extend([(ii+1,jj),(ii-1,jj),(ii,jj-1),(ii,jj+1)])
+    return count
+
+    ## recursive DFS
+    def dfs(grid, i,j):
+        if 0<=i<m and 0<=j<n and grid[i][j] == "1":
+            grid[i][j] = "2"
+            dfs(grid, i-1, j)
+            dfs(grid, i+1, j)
+            dfs(grid, i, j-1)
+            dfs(grid, i, j+1)
+    if not grid:
+        return 0
+    m, n, count = len(grid), len(grid[0]), 0
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == "1":
+                count += 1
+                dfs(grid, i, j)
+    return count*/
