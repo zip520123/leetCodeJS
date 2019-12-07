90368d300b6699b86ab317c6176e6af580ab49bc
zip520123 <zip520123@yahoo.com.tw>
2019-12-07 14:15:23 +0000

    add number of islands

---
+++ tree/medium/Number of Islands.js
@@ -0,0 +1,47 @@
+/*Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3*/
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+var numIslands = function(grid) {
+  //each islend became sea
+  res = 0
+  for(i=0;i<grid.length;i++){
+    for(j=0;j<grid[i].length;j++){
+      if (grid[i][j] == 1) {
+        res ++
+        bfs(grid,i,j)
+      } 
+    }
+  }
+  return res
+}
+const bfs = (grid,i,j) => {
+  if (i < 0 || j < 0 || i >= grid.length || j >=grid[i].length || grid[i][j] == 0) return
+  grid[i][j] = 0
+  bfs(grid,i-1,j)
+  bfs(grid,i,j+ 1)
+  bfs(grid,i+1,j)
+  bfs(grid,i,j-1)
+}
