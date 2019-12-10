09a4c19349edf9e486d91457781fe59ecadaf969
zip520123 <zip520123@yahoo.com.tw>
2019-12-10 11:10:14 +0000

    add product of array except self

--- array/hard/Product of Array Except Self.js
+++ array/hard/Product of Array Except Self.js
@@ -47,3 +47,19 @@ var productExceptSelf = function(nums) {
   }
   return res
 }
+
+//time O(N)
+//space O(N)
+
+var productExceptSelf = function (nums) {
+  res = [1]
+  for(i=1;i<nums.length;i++){
+    res[i] = res[i-1] * nums[i-1]
+  }
+  r = 1
+  for(i=nums.length - 1;i>=0;i--){
+    res[i] = res[i] * r
+    r *= nums[i]
+  }
+  return res
+}
